57662d83f4096205d8bb84656aa3434ff7a7c26d
ronin <crazzy-rock@live.com.mx>
2015-12-20 22:23:39 -0600

    agregando archivo sql persona

---
+++ persona.sql
@@ -0,0 +1,9 @@
+CREATE TABLE persona(
+	id_persona 				int(11) not null,
+	nombre					varchar(45),
+	apellido_paterno		varchar(45),
+	apellido_materno		varchar(45),
+	email					varchar(45),
+	telefoono				varchar(45),
+	PRIMARY KEY(id_persona)
+);
